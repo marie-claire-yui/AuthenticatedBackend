784382de3e1d31efbccf4be86708e35f7e438077
marie-claire-yui <marieclairekasai@gmail.com>
2023-07-21 16:45:29 +0200

    a

--- src/main/java/com/unknownkoder/configuration/SecurityConfiguration.java
+++ src/main/java/com/unknownkoder/configuration/SecurityConfiguration.java
@@ -6,16 +6,33 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.unknownkoder.utils.RSAKeyProperties;
 
 
 
 @Configuration
 public class SecurityConfiguration {
+
+    private final RSAKeyProperties keys;
+
+    public SecurityConfiguration(RSAKeyProperties keys){
+        this.keys = keys;
+    }
     
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -43,4 +60,17 @@ public class SecurityConfiguration {
             .httpBasic().and()
             .build();
     } 
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(keys.getPublicKey()).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(){
+        JWK jwk = new RSAKey.Builder(keys.getPublicKey()).privateKey(keys.getPrivateKey()).build();
+        JWKSource<SecurityContext> jwks = new JWKSource<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
 }
